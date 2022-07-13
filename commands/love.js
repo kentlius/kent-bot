4260905270ab03621534a6a726bf7e8806738665
kentlius <kent.delatinos@gmail.com>
2022-07-13 18:00:40 +0700

    add love calc

---
+++ commands/love.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('love')
+		.setDescription('Love Calculator')
+        .addStringOption(option => option.setName('orang_1').setDescription('tumbal 1'))
+        .addStringOption(option => option.setName('orang_2').setDescription('tumbal 2')),
+	async execute(interaction) {
+        const orang_1 = interaction.options.getString('orang_1');
+        const orang_2 = interaction.options.getString('orang_2');
+        const response = await axios.get(`https://api.genderize.io/?name[]=${orang_1}&name[]=${orang_2}&country_id=ID`);
+        const gender_1 = await response.data[0].gender;
+        const gender_2 = await response.data[1].gender;
+        await interaction.deferReply();
+        if (gender_1 == gender_2) {
+            return interaction.editReply('sus ඞ');
+        }
+		return interaction.editReply(Math.floor(Math.random() * 100) + 1 + '%');
+	},
+};
