b64fbd987ed2d0f2aba8aa78396d7757d6721867
kentlius <kent.delatinos@gmail.com>
2022-07-13 13:12:13 +0700

    add cat and urban command

---
+++ commands/cat.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { request } = require('undici');
+const getJSONResponse = require('../utils/getJSONResponse');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('cat')
+		.setDescription('Cat pics!'),
+	async execute(interaction) {
+		const catResult = await request('https://aws.random.cat/meow');
+		const { file } = await getJSONResponse(catResult.body);
+        await interaction.deferReply();
+		return interaction.editReply({ files: [file] });
+	},
+};
